769367604dbee06f9feb572bf822687abf37c5da
Sumit Kumar <kumarsumitjat298@gmail.com>
2022-04-17 14:48:24 +0530

    abccd

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -17,36 +17,6 @@ function Nav() {
         userService.logout();
     }
 
-<<<<<<< HEAD
-  // only show nav when logged in
-  if (!user) return null;
-
-  return (
-    <nav>
-      <div className="flex flex-row gap-5 text-xl bg-black text-white p-5">
-        <NavLink href="/" exact className="">
-          Home
-        </NavLink>
-
-        {userService.userValue.role == "admin" && (
-          <NavLink href="/users" className="">
-            Users
-          </NavLink>
-        )}
-        <NavLink href="/notification" className="">
-          notifications
-        </NavLink>
-        <a onClick={logout} className="">
-          Logout
-        </a>
-        <h1 className="text-xl font-bold text-green-600">
-          Hi {userService.userValue?.firstName}! ({userService.userValue?.ErNo})
-        </h1>
-      </div>
-    </nav>
-  );
-}
-=======
     // only show nav when logged in
     if (!user) return null;
     
@@ -60,4 +30,4 @@ function Nav() {
         </nav>
     );
 }
->>>>>>> parent of bde5b2e (installed jshint and created frontend pages for student and groups)
+
